4cb0b798a09bf2fa77f3aa15cf3016598ae91785
Vladislav Igorevich <cool-smit3@yandex.ru>
2022-04-25 23:13:36 +0300

    AddedQuest203

--- Quest203/Program.cs
+++ Quest203/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Напишите программу, которая принимает на вход 
+// координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+double length(double x1, double y1, double x2, double y2)
+{
+    // x1 = 1, y1 = 1
+    // x2 = 3, y2 = 5
+    double deltaX = Math.Abs(x1 - x2);
+    double deltaY = Math.Abs(y1 - y2);
+    double deltaXY = Math.Sqrt((deltaX * deltaX) + (deltaY * deltaY));
+    return deltaXY;
+}
+
+double ax = 3;
+double ay = 2;
+double bx = 6;
+double by = 1;
+
+Console.WriteLine("lenght " + Math.Round(length(ax, ay, bx, by),2));
+
+*/
+
+
