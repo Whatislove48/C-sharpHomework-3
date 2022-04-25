1fddcc6bab3c41f3e7a1c6da5f5a5a8b0266a4e9
Vladislav Igorevich <cool-smit3@yandex.ru>
2022-04-26 00:38:27 +0300

    AddedQuest204

---
+++ Quest204/Program.cs
@@ -0,0 +1,48 @@
+﻿
+// Напишите программу, которая принимает на
+ // вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+int Palindrom(int[] num){
+    int flag = 0;
+    for (int i = 0; i < (4-i); i++){
+        if(num[i] == num[4-i]){
+            flag = 1;
+        }
+        else{
+            flag = 0;
+            break;
+        }
+    }
+    return flag;
+}
+
+int[] CountOfNumber(int numeric){
+    int[] NumericCount = new int [5]; 
+    for (int i = 0;i < 5 ;i++){
+        NumericCount[i] = (numeric%10);
+        numeric /= 10;                                      
+    } 
+    return NumericCount;           
+}
+
+
+int testnum = 12821;
+int id = Palindrom(CountOfNumber(testnum));
+if (testnum < 100000 && testnum > 9999){
+    if (id == 1){
+        Console.WriteLine("Yes");
+    }
+    else {
+        Console.WriteLine("No");
+    }
+} 
+else{
+    Console.WriteLine("Число не 5ти значное");
+    }
+
+
+
+
